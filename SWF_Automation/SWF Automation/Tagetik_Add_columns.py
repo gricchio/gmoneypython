f55b4a297d2df25b9f77038073272da75a12be68
Gino Work <riccga@LAP27266.springswf.com>
2017-10-10 16:42:18 -0500

    Working

--- SWF_Automation/SWF Automation/Tagetik_Add_columns.py
+++ SWF_Automation/SWF Automation/Tagetik_Add_columns.py
@@ -10,13 +10,20 @@ cpu_files = r'C:\Users\riccga\Desktop\Python Exports\Tagetik\Facility Review Fil
 workbook = r'C:\Users\riccga\Desktop\Python Exports\Tagetik\Facility Review Files\Reynosa 1 Facility Reviews.xlsm'
 
 sheets = ['Assembly Actuals','Component Actuals']
-insert_list = ['C','E','G','J','L','O','R','T','W']
+#col_to_insert = ['C','E','G','J','L','O','R','T','W']
+col_to_insert = [3,5,7,10,12,15,18,20,23]
+assembly_unit_of_measure_row = 14
+chart_gap  = 92
+
+rows_needed_for_formulas = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,72,73,74,78,79,80,81,82,86,88,92,93]
+
 
 def insert_format(col):
-    for col in insert_list:
+    for col in col_to_insert:
         ws.Columns(col).EntireColumn.Insert()
         ws.Columns(col).ColumnWidth = 7.71
         ws.Columns(col).Font.ColorIndex = 16
+        ws.Columns(col).NumberFormat = "0.00"
 
 
 #Change to project dir
@@ -30,20 +37,28 @@ xl.DisplayAlerts = 'false'
 
 xl.Workbooks.Open(workbook)
 
+
 ws = xl.Worksheets('Assembly Actuals')
 
 
 #insert columns
+"""
 for sheet in sheets:
     ws = xl.Worksheets(sheet)
-    insert_format(insert_list)
-    
-ws.Columns("C").Copy()
-ws.Columns("O").PasteSpecial(Paste= "xlPasteFormats")
-
-
+    insert_format(col_to_insert)
 
 """
+
+ws.Cells(3,8).Formula = "=iferror(" + str() 
+
+"""
+for columns in col_to_insert:
+    for row in rows_needed_for_formulas:
+        
+
+
+
+
 xl.ActiveSheet.Cells(2,7).Value = "Testing"
 ws.Cells(3,8).Value = ":-)"
 ws.Columns("B").EntireColumn.Insert()
--- SWF_Automation/SWF Automation/windowswitching.py
+++ SWF_Automation/SWF Automation/windowswitching.py
@@ -3,8 +3,14 @@ Created on Oct 3, 2017
 
 @author: riccga
 '''
+col_to_insert = ['C','E','G','J','L','O','R','T','W']
 
-from pywinauto import application
+assembly_unit_of_measure_row = 14
+chart_gap  = 92
 
-app = application.Application()
-app.start(r"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE")
+rows_needed_for_formulas = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,72,73,74,78,79,80,81,82,86,88,92,93]
+
+
+for column in col_to_insert:
+    for row in rows_needed_for_formulas:
+        print column + str(row)

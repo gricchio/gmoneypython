b4d66ab0cd4b1b11a7f929d1430cee5cd847e52e
Gino Work <riccga@LAP27266.springswf.com>
2017-10-11 12:07:10 -0500

    Final version

--- SWF_Automation/SWF Automation/Tagetik_Add_columns.py
+++ SWF_Automation/SWF Automation/Tagetik_Add_columns.py
@@ -15,6 +15,7 @@ col_before = ['B','D','F','I','K','N','Q','S','V']
 #col_to_insert = [3,5,7,10,12,15,18,20,23]
 assembly_unit_of_measure_row = 14
 chart_gap  = 92
+number_of_charts = 10
 
 rows_needed_for_formulas = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,72,73,74,78,79,80,81,82,86,88,92,93]
 
@@ -30,32 +31,39 @@ def insert_format(col):
 #Change to project dir
 os.chdir(cpu_files)
 
+#build file dispatch
+
+files = []
+for name in os.listdir(cpu_files):
+    if os.path.isfile(os.path.join(cpu_files, name)):
+        files.append(name)
+
+print files
+
 #dispatch Excel
 
 xl = win32com.client.Dispatch("Excel.Application")
 xl.Visible = 1
 xl.DisplayAlerts = 'false'
 
-xl.Workbooks.Open(workbook)
 
-
-ws = xl.Worksheets('Assembly Actuals')
-
-
-#insert columns
-
-for sheet in sheets:
-    ws = xl.Worksheets(sheet)
-    insert_format(col_to_insert)
-
-for sheet in sheets:
-    ws = xl.Worksheets(sheet)
-    for column in col_to_insert:
-        for row in rows_needed_for_formulas:
-            ws.Range(column + str(row)).Formula = "=iferror(offset(" + column + str(row)+ ",0,-1)" + "/" + "offset(" + column + str(assembly_unit_of_measure_row) + ",0,-1)" +",0)"    
+for file in files:
+    wb = xl.Workbooks.Open(os.path.join(cpu_files,file))
     
+#insert columns / Formatting
 
-
+    for sheet in sheets:
+        ws = xl.Worksheets(sheet)
+        insert_format(col_to_insert)
+    for sheet in sheets:
+        ws = xl.Worksheets(sheet)
+        for x in range(0,number_of_charts):
+            for column in col_to_insert:
+                for row in rows_needed_for_formulas:
+                    ws.Range(column + str(row + (x*chart_gap))).Formula = "=iferror(offset(" + column + str(row + (x*chart_gap)) + ",0,-1)" + "/" + "offset(" + column + str(assembly_unit_of_measure_row + (x*chart_gap)) + ",0,-1)" +",0)"   
+    wb.Close(True)  
+    
+    
 
 """
 
--- SWF_Automation/SWF Automation/windowswitching.py
+++ SWF_Automation/SWF Automation/windowswitching.py
@@ -1,16 +1,10 @@
-'''
-Created on Oct 3, 2017
+import os
+path = r'C:\Users\riccga\Desktop\Python Exports\Tagetik\Facility Review Files'
 
-@author: riccga
-'''
-col_to_insert = ['C','E','G','J','L','O','R','T','W']
-
-assembly_unit_of_measure_row = 14
-chart_gap  = 92
-
-rows_needed_for_formulas = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,72,73,74,78,79,80,81,82,86,88,92,93]
+files = []
+for name in os.listdir(path):
+    if os.path.isfile(os.path.join(path, name)):
+        files.append(name)
 
 
-for column in col_to_insert:
-    for row in rows_needed_for_formulas:
-        print column + str(row)
+print files

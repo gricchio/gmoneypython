6af81f64574aac1a6cc93c1a1208ee0a47fb5ed2
Gino Work <riccga@LAP27266.springswf.com>
2017-12-11 11:01:59 -0600

    got all the way to the enter screen

--- SWF_Automation/SWF Automation/Internet_explorer_webdriver.py
+++ SWF_Automation/SWF Automation/Internet_explorer_webdriver.py
@@ -15,14 +15,37 @@ def press(x):
     keyboard.release(x)
 keyboard = Controller()
 
+def xpath_select_click(x):
+    y = driver.find_element_by_xpath(x)
+    y.click()
+    time.sleep(5)
+    
+def xpath_click(x):
+    y = driver.find_element_by_xpath(x)
+    y.click()
 
 service = r'C:\Users\riccga\Desktop\Python Exports\Github\gmoneypython\SWF_Automation\WebInteract\Drivers\IEDriverServer.exe'
-
 url = 'https://springswindowfashions.saastagetik.com/prod/5#!/PROD_TGK_SPRINGSWINDOWFASHIONS_001'
+
+curr_Scenario = '2017ACT - 2017 ACTUAL'
+curr_pd = '11 - November'
 
 #xpaths
 
+#   Main Menus
+forecast_submenu = r'//*[@id="tgk.main.workarea"]/div/div/div/div/div/div/div/div[1]/div/div/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div/div/div/div/div[1]/div'
+actual_submenu = r'//*[@id="tgk.main.workarea"]/div/div/div/div/div/div/div/div[1]/div/div/div[3]/div/div[2]/div/div[2]/div[1]/div[2]/div/div/div/div/div[1]/div'
 
+## Actual Selections
+
+midd_actuals = r'//*[@id="web.vaadin.ui.components.DefaultTgkWindow.mainLayout"]/div/div[3]/div/div/div[3]/div/div[2]/div[1]/table/tbody/tr[12]/td[2]/div/div/div/div[2]'
+
+
+###Actuals Menu
+
+actuals_Scenario = r'//*[@id="gwt-uid-58"]/div/div/div/div/div/input'
+actuals_PD = r'//*[@id="gwt-uid-60"]/div/div/div/div/div/input'
+actuals_menu = [actuals_Scenario, actuals_PD]
 
 
 driver = webdriver.Ie(service)
@@ -34,8 +57,23 @@ for i in taco:
     time.sleep(3)
     press(Key.enter)
 
-time.sleep(5)
+time.sleep(8)
 
-forecasting = driver.find_element_by_xpath(r'//*[@id="tgk.main.workarea"]/div/div/div/div/div/div/div/div[1]/div/div/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div/div/div/div/div[1]/div')
+##forecasting = driver.find_element_by_xpath(actual_submenu)
+##forecasting.click()
 
-forecasting.click()
+process = [actual_submenu, midd_actuals]
+
+for i in process:
+    xpath_select_click(i)
+
+time.sleep(3)
+xpath_click(actuals_Scenario)
+
+keyboard.type(curr_Scenario)
+
+xpath_click(actuals_PD)
+keyboard.type(curr_pd)
+press(Key.enter)
+
+

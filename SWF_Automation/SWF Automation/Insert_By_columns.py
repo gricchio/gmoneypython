e281da83a8032c819b4808546a3450513f559837
Gino Work <riccga@LAP27266.springswf.com>
2018-02-02 12:13:40 -0600

    Finished product

---
+++ SWF_Automation/SWF Automation/Insert_By_columns.py
@@ -0,0 +1,41 @@
+'''
+Created on Jan 31, 2018
+
+@author: riccga
+'''
+
+import os
+import pandas as pd
+import win32com.client
+
+SourceFile = r'C:\Users\riccga\Desktop\Python Exports\Insert Values'
+os.chdir(SourceFile)
+depts = pd.read_excel("Tag_Dept_Cheatsheet.xlsx")
+destination = os.path.join(SourceFile,"Destination.xlsx")
+Starting_Cell = "B3"
+number_of_rows_to_skip = 5
+
+
+
+listofdepts = []
+
+for item in depts["Dept"]:
+    listofdepts.append(item)
+
+
+"""   
+#dispatch Excel
+
+xl = win32com.client.Dispatch("Excel.Application")
+xl.Visible = 1
+xl.DisplayAlerts = 'false'
+dest = xl.Workbooks.Open(destination)
+"""
+
+dest = win32com.client.GetActiveObject("Excel.Application")
+insert_cell= Starting_Cell
+dest.Range(insert_cell).Select()
+for dept in listofdepts:
+    dest.ActiveCell.Formula = str(dept)
+    dest.ActiveCell.Offset(number_of_rows_to_skip+2,1).Select()
+    

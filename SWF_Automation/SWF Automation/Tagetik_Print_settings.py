7fe7b1d21cc5d16aa7408a357c91657aa555a8f9
Gino Work <riccga@LAP27266.springswf.com>
2018-03-08 18:35:08 -0600

    print settings

--- SWF_Automation/SWF Automation/Tagetik_Print_settings.py
+++ SWF_Automation/SWF Automation/Tagetik_Print_settings.py
@@ -6,33 +6,48 @@ Created on Feb 26, 2018
 import os
 import win32com.client
 
+location = r'C:\Users\riccga\Desktop\Python Exports\Print Settings'
 
-workbook = 
+print_area = "B6:X925"
 
-#create an archive to see what is in the folded -- Need to change name to move files
+
+#print folder
+
+os.chdir(location)
 
 files = []
-os.chdir(moveFiles)
-for name in os.listdir(moveFiles):
-    if os.path.isfile(os.path.join(moveFiles, name)):
+os.chdir(location)
+for name in os.listdir(location):
+    if os.path.isfile(os.path.join(location, name)):
         files.append(name)
-for name in os.listdir(recieveFiles):
-    if os.path.isfile(os.path.join(recieveFiles, name)):
-        files.append(name)
-
-
-
-
 print files
 
+files = os.path.join(location, files[0])
 
 #Dispatch Excel
-
+#"""
 xl = win32com.client.Dispatch("Excel.Application")
 xl.Visible = 1
 xl.DisplayAlerts = 'false'
 
 #Open workbook
+wb = xl.Workbooks.Open(files)
+
+"""
+
+#If Excel is already Running -------
+
+wb = win32com.client.GetObject(files)
+
+#If Excel is already Running -------
+"""
+ws = wb.Worksheets("Assembly Actuals")
+
+ws.View = "xlPageBreakPreview"
+                   
+ws.PageSetup.PrintArea = print_area
 
 
-wb = xl.Workbooks.Open(workbook)
+ws.Rows("97:97").Select
+
+#ws.ActiveWindow.SelectedSheets.HPageBreaks.Add

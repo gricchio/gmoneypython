e904981dab0193700b77bc7a9e099f095cb36141
Gino - Gaming PC <ginoricchio@live.com>
2018-10-15 18:09:18 -0500

    Creation

---
+++ SPB_Automation/SPB_PI.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 15, 2018
+
+@author: Gino Ricchio
+'''
+import win32com.client
+
+print "hello Gino"
--- SWF_Automation/SWF Automation/Tagetik_modify.py
+++ SWF_Automation/SWF Automation/Tagetik_modify.py
@@ -3,3 +3,5 @@ Created on Sep 15, 2017
 
 @author: RiccGA
 '''
+
+print "hello world"

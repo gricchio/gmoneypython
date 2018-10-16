1f605d1c1b0d1044816c4ef76dfd4425fc84c27b
200460 <200460@LPF12X5MN.rayovac.com>
2018-10-16 12:51:09 -0500

    Finished Push

--- SPB_Automation/SPB_PI.py
+++ SPB_Automation/SPB_PI.py
@@ -4,17 +4,18 @@ import pandas as pd
 
 import os
 import xlwings as xw
-import win32com.client
+
 
 
 #-------------Project Locations
 
-#pi_template = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price Increase Sample.xlsx'
+pi_template = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price Increase Sample.xlsx'
+price_data_location = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price_data_real.xlsx'
 #price_data_location = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price_data_test.xlsx'
-#project_path = r'C:\Users\200460\Desktop\Python Projects\Price List Project'
-price_data_location = r'C:\Users\Gino Ricchio\Desktop\Python\Price_data_test.xlsx'
-project_path = r'C:\Users\Gino Ricchio\Desktop\Python\Completed'
-pi_template = r'C:\Users\Gino Ricchio\Desktop\Python\Price Increase Sample.xlsx'
+project_path = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Completed'
+#price_data_location = r'C:\Users\Gino Ricchio\Desktop\Python\Price_data_test.xlsx'
+#project_path = r'C:\Users\Gino Ricchio\Desktop\Python\Completed'
+#pi_template = r'C:\Users\Gino Ricchio\Desktop\Python\Price Increase Sample.xlsx'
 
 #-------------Move to Project Path
 
@@ -23,26 +24,28 @@ os.chdir(project_path)
 #-------------Load Data Frame
 
 increase_master = pd.ExcelFile(price_data_location)
-df = pd.read_excel(increase_master, "Sheet1")
+master = pd.read_excel(increase_master, "CustomerMaterial")
+
 soldtos = []
-for i in df['Soldto'].values:
+for i in master['Customer'].values:
     if i not in soldtos:
         soldtos.append(i)
 
-df = df.set_index(['Soldto'])
-df['Delta'] = df['NEW'] - df['OLD']
-df['Prc CHG'] = df['Delta'] / df['OLD']
+master = master.set_index(['Customer'])
 
-df['OLD'] = df['OLD'].round(2)
-df['Delta'] = df['Delta'].round(2)
-df['Prc CHG'] = df['Prc CHG'].round(2)
-
-for account_name in soldtos:
+for account_name in soldtos[0:3]:
     print "Now working on customer number " + str(account_name)
     wb = xw.Book(pi_template)
     ws = wb.sheets[0]
     ws.range('C9').value = account_name
     ws.range('C10').value = account_name
-    ws.range('B13').options(index=False, header=False).value = df.loc[account_name]
+    #need to add 'UPC' in lieu of DCHL
+    df2 = master.loc[[account_name], ['Item','DChl', 'Description', 'UOM', 'PR00', 'NEW']]
+    df2['Delta'] = df2['NEW'] - df2['PR00']
+    df2['Prc CHG'] = df2['Delta'] / df2['PR00']
+    df2['PR00'] = df2['PR00'].round(2)
+    df2['Delta'] = df2['Delta'].round(2)
+    df2['Prc CHG'] = df2['Prc CHG'].round(2)
+    ws.range('B13').options(index=False, header=False).value = df2.loc[[account_name]]
     wb.save(os.path.join(project_path,str(account_name) + ".xlsx"))
-#xw.apps[0].quit()
+    wb.close()

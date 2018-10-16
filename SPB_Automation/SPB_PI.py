e94ce46cd87618963f19889b71bcdaabdcb22f7d
200460 <200460@LPF12X5MN.rayovac.com>
2018-10-15 22:14:37 -0500

    update 1

--- SPB_Automation/SPB_PI.py
+++ SPB_Automation/SPB_PI.py
@@ -7,11 +7,11 @@ import os
 import win32com.client
 
  
-
-#price_data_location = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price_data_test.xlsx'
-#project_path = r'C:\Users\200460\Desktop\Python Projects\PrList Project'
-price_data_location = r'C:\Users\Gino Ricchio\Desktop\Python\Price_data_test.xlsx'
-project_path = r'C:\Users\Gino Ricchio\Desktop\Python'
+pi_template = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price Increase Sample.xlsx'
+price_data_location = r'C:\Users\200460\Desktop\Python Projects\Price List Project\Price_data_test.xlsx'
+project_path = r'C:\Users\200460\Desktop\Python Projects\Price List Project'
+#price_data_location = r'C:\Users\Gino Ricchio\Desktop\Python\Price_data_test.xlsx'
+#project_path = r'C:\Users\Gino Ricchio\Desktop\Python'
 
 
 os.chdir(project_path)
@@ -24,11 +24,17 @@ for i in df['Soldto'].values:
         soldtos.append(i)
 
 print df.head(20)
-
-df = df.set_index(['Soldto'])
-
-
-#print df.loc[912019]
 print soldtos
+df = df.set_index(['Soldto'])
+df['Delta'] = df['NEW'] - df['OLD']
+#print df.loc[912019]
+
+print "this is Gino"
+"""
+xl = win32com.client.Dispatch("Excel.Application")
+xl.Visible = 1
+xl.DisplayAlerts = 'false'
+wb = xl.Workbooks.Open(pi_template)
+"""
 
 

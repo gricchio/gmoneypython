d2edfbebc4247f64b5a391131fc76deb24b2c2d1
Gino - Gaming PC <ginoricchio@live.com>
2019-02-17 23:51:25 -0600

    CHPT12

---
+++ Black Hat/Black_Hat/UDP.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 17, 2019
+
+@author: Gino Ricchio
+'''
+import socket
+
+target_host = "127.0.0.1"
+
+target_port = 80
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind((target_host, target_port))
+client.sendto("AAABBBCCC", (target_host,target_port))
+
+data, addr = client.recvfrom(4096)
+
+print data

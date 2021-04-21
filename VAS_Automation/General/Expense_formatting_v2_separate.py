c05e016d9a365d29cb7657a9cf57789bbe66ec90
gricchio <ginoricchio@live.com>
2021-04-21 18:58:24 -0500

    final one

--- VAS_Automation/General/Expense_formatting_v2_separate.py
+++ VAS_Automation/General/Expense_formatting_v2_separate.py
@@ -22,6 +22,38 @@ break_out = ['PD - Kevin',
             'WAREHOUSE'
             ]
 
+
+#email list
+break_out2 = {"PD - Kevin" : "gino.ricchio@vas.com",
+            "BUS DEV": "ginoricchio@gmail.com",
+            "EQUIPMENT": "gino.ricchio@vas.com",
+            "FIELD MARKETING": "gino.ricchio@vas.com",
+            "FINANCE": "gino.ricchio@vas.com",
+            "HR": "gino.ricchio@vas.com",
+            "IT": "gino.ricchio@vas.com",
+            "MANAGEMENT": "gino.ricchio@vas.com",
+            "SALES": "gino.ricchio@vas.com",
+            "SUPPORT": "gino.ricchio@vas.com",
+            "WAREHOUSE": "gino.ricchio@vas.com"
+            }
+
+#send function
+import win32com.client
+
+def message_email_self(Message):
+    outlook = win32com.client.Dispatch('outlook.application')
+    workemail = break_out2[Message]
+    mail = outlook.createitem(0)
+    mail.To = workemail
+    mail.Subject = 'VAS Expense Overview - Previous Month'
+    mail.body = "Hello, please see the attached document for your monthly expenses. I look forward to reviewing with you in the near future. Talk soon, Gino"
+    attachment1 = str(r"C:\Users\gino.ricchio\Desktop\Python Projects\Expense Separation\New Items/"+ Message + ".xlsx")
+    print(attachment1)
+    mail.Attachments.Add(Source=attachment1)
+    mail.send
+    print("Message Sent")
+
+
 #Code
 
 os.chdir(project_folder)
@@ -48,5 +80,6 @@ for dept in break_out:
     new.sheets['Sheet1'].delete()
     new.save(str(dept + '.xlsx'))
     new.close()
+    message_email_self(dept)
 
 

6b5a1583805f640b1616ec84c830a19a84c54742
gricchio <ginoricchio@live.com>
2021-04-21 19:07:01 -0500

    Completed

--- VAS_Automation/General/Expense_formatting_v2_separate.py
+++ VAS_Automation/General/Expense_formatting_v2_separate.py
@@ -45,7 +45,7 @@ def message_email_self(Message):
     workemail = break_out2[Message]
     mail = outlook.createitem(0)
     mail.To = workemail
-    mail.Subject = 'VAS Expense Overview - Previous Month'
+    mail.Subject = 'VAS Expense Overview - ' + Message
     mail.body = "Hello, please see the attached document for your monthly expenses. I look forward to reviewing with you in the near future. Talk soon, Gino"
     attachment1 = str(r"C:\Users\gino.ricchio\Desktop\Python Projects\Expense Separation\New Items/"+ Message + ".xlsx")
     print(attachment1)
